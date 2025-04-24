d467ce2fbe60231891e74f417a9d022754caa2f2
Chetan Ranpariya <ranpariyachetan@users.noreply.github.com>
2025-04-24 23:55:57 +0800

    Started creating streamlit app.

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
+import streamlit as st
 import ytchatter
 from ytchatter import YTChatter
 
 
 video_id="hF-7eKtzAHM"
 chatter = YTChatter()
-chatter.start_chatting(video_id)
 
-question = "What is the main topic of the video?"
-res = chatter.ask_question(question)
+st.set_page_config(page_title="YT Chatter", page_icon=":guardsman:", layout="wide")
 
-print(res)
+st.write(
+    "This is a simple app that allows you to ask questions about a YouTube video and get answers using the OpenAI API."
+)
+
+video_id = st.text_input(
+    "Enter the YouTube video ID",
+    placeholder="Enter the YouTube video ID",
+    key="video_id",
+)
+
+if video_id:
+    chatter.start_chatting(video_id)
+    st.chat_input(
+        "Ask a question about the video",
+        key="question",
+    )
+else:
+    if "video_id" not in st.session_state:
+        st.session_state.video_id = ""
