fdab64ab87e021a313979d468b6b3686e45a08a2
Chetan Ranpariya <ranpariyachetan@users.noreply.github.com>
2025-05-01 00:22:41 +0800

    Improved performance of the chatter by using session state.

--- app.py
+++ app.py
@@ -11,6 +11,9 @@ chat_history = StreamlitChatMessageHistory()
 
 chatter = YTChatter()
 
+if "chatter" not in st.session_state:
+    st.session_state.chatter = chatter
+
 if "llm" not in st.session_state:
     st.session_state.llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
 
@@ -23,10 +26,13 @@ st.write(
     "This is a simple app that allows you to ask questions about a YouTube video and get answers using the OpenAI API."
 )
 
+if "video_id" not in st.session_state:
+    st.session_state.video_id = ""
+
 video_info = st.text_input(
     "Enter the YouTube video ID",
     placeholder="Enter the YouTube video URL/ID",
-    key="video_id",
+    key="video_info",
 )
 
 for message in st.session_state.conversation:
@@ -34,14 +40,24 @@ for message in st.session_state.conversation:
         st.markdown(message["content"])
 
 if video_info:
+    
     video_id = extract_video_id(video_info)
     if video_id:
-        transcripts = get_video_transcript(video_id)
-        
-        if transcripts:
-            vector_store = create_vector_store(transcripts, video_id)
-            chatter.start_chatting(vector_store, st.session_state.llm)
+        if video_id != st.session_state.video_id:
+            st.session_state.video_id = video_id
+            st.session_state.canChat = False
+            st.session_state.conversation = []
 
+            transcripts = get_video_transcript(video_id)
+
+            if transcripts:
+                vector_store = create_vector_store(transcripts, st.session_state.video_id)
+                st.session_state.chatter.start_chatting(vector_store, st.session_state.llm)
+                st.session_state.canChat = True
+            else:
+                st.session_state.canChat = False
+            
+        if st.session_state.canChat:
             question = st.chat_input(
                 "Ask a question about the video",
                 key="question",
@@ -50,11 +66,11 @@ if video_info:
                 st.chat_message("user").markdown(question)
                 st.session_state.conversation.append({"role": "user", "content": question})
                 with st.spinner("Thinking..."):
-                    answer = chatter.ask_question(question)
+                    answer = st.session_state.chatter.ask_question(question)
 
                 with st.chat_message("assistant"):
                     st.markdown(answer)
-                st.session_state.conversation.append({"role": "assistant", "content": answer})
+                    st.session_state.conversation.append({"role": "assistant", "content": answer})
         else:
             st.error("Could not retrieve information for the provided video ID.")
     else:
